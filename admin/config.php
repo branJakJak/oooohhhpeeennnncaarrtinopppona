17d1a33e3ec2fea5c7ae84dc1ed25fc6b33ec1c3
Your Name <you@example.com>
2016-05-23 13:05:26 +0100

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.innovape.co/admin/');
+define('HTTP_CATALOG', 'http://opencart.innovape.co/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.innovape.co/admin/');
+define('HTTPS_CATALOG', 'http://opencart.innovape.co/');
+
+// DIR
+define('DIR_APPLICATION', '/home/innovape/public_html/opencart/admin/');
+define('DIR_SYSTEM', '/home/innovape/public_html/opencart/system/');
+define('DIR_IMAGE', '/home/innovape/public_html/opencart/image/');
+define('DIR_LANGUAGE', '/home/innovape/public_html/opencart/admin/language/');
+define('DIR_TEMPLATE', '/home/innovape/public_html/opencart/admin/view/template/');
+define('DIR_CONFIG', '/home/innovape/public_html/opencart/system/config/');
+define('DIR_CACHE', '/home/innovape/public_html/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/innovape/public_html/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/innovape/public_html/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/innovape/public_html/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/innovape/public_html/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/home/innovape/public_html/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'innovape_opencrt');
+define('DB_PASSWORD', 'hitman052529');
+define('DB_DATABASE', 'innovape_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.innovape.co/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.innovape.co/');
+
+// DIR
+define('DIR_APPLICATION', '/home/innovape/public_html/opencart/catalog/');
+define('DIR_SYSTEM', '/home/innovape/public_html/opencart/system/');
+define('DIR_IMAGE', '/home/innovape/public_html/opencart/image/');
+define('DIR_LANGUAGE', '/home/innovape/public_html/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/home/innovape/public_html/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/home/innovape/public_html/opencart/system/config/');
+define('DIR_CACHE', '/home/innovape/public_html/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/innovape/public_html/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/innovape/public_html/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/innovape/public_html/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/innovape/public_html/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'innovape_opencrt');
+define('DB_PASSWORD', 'hitman052529');
+define('DB_DATABASE', 'innovape_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
