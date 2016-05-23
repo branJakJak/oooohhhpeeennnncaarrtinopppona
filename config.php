7be5da0105fc2fbe1ed04558e68ff24efc067be3
Your Name <you@example.com>
2016-05-24 02:06:55 +0800

    ds

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart.innovape.co/');
+define('HTTP_SERVER', 'http://localhost:8000/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart.innovape.co/');
+define('HTTPS_SERVER', 'http://localhost:8000/');
 
 // DIR
-define('DIR_APPLICATION', '/home/innovape/public_html/opencart/catalog/');
-define('DIR_SYSTEM', '/home/innovape/public_html/opencart/system/');
-define('DIR_IMAGE', '/home/innovape/public_html/opencart/image/');
-define('DIR_LANGUAGE', '/home/innovape/public_html/opencart/catalog/language/');
-define('DIR_TEMPLATE', '/home/innovape/public_html/opencart/catalog/view/theme/');
-define('DIR_CONFIG', '/home/innovape/public_html/opencart/system/config/');
-define('DIR_CACHE', '/home/innovape/public_html/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/innovape/public_html/opencart/system/storage/download/');
-define('DIR_LOGS', '/home/innovape/public_html/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/innovape/public_html/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/home/innovape/public_html/opencart/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/wamp/www/OpenCart/upload/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/OpenCart/upload/system/');
+define('DIR_IMAGE', 'C:/wamp/www/OpenCart/upload/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/OpenCart/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/OpenCart/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/OpenCart/upload/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/OpenCart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/OpenCart/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/OpenCart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/OpenCart/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/OpenCart/upload/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'innovape_opencrt');
-define('DB_PASSWORD', 'hitman052529');
-define('DB_DATABASE', 'innovape_opencart');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
